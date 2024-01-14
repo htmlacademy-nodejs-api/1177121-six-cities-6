b9076580e1a38a89bed4da7b92307a864d5ec131
RayFreedom88 <captain.musin@yandex.ru>
2024-01-14 23:08:50 +0200

    1.16 Раскрасит вывод информации в консоли при помощи Chalk

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { ICommand } from './command.interface.js';
 
 export class HelpCommand implements ICommand {
@@ -7,14 +8,15 @@ export class HelpCommand implements ICommand {
 
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
-        Программа для подготовки данных для REST API сервера.
-        Пример:
-            cli.js --<command> [--arguments]
-        Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+        ${chalk.bold.magentaBright('Программа для подготовки данных для REST API сервера.')}
+
+        ${chalk.white('Пример:')} ${chalk.yellowBright('cli.js --<command> [--arguments]')}
+
+        ${chalk.white('Команды:')}
+            ${chalk.green('--version:')}                   # выводит номер версии
+            ${chalk.green('--help:')}                      # печатает этот текст
+            ${chalk.green('--import <path>: ')}            # импортирует данные из TSV
+            ${chalk.green('--generate <n> <path> <url>')}  # генерирует произвольное количество тестовых данных
     `);
   }
 }
