dcfb1f5cde342990f972a1631080efde7520b693
RayFreedom88 <captain.musin@yandex.ru>
2024-01-14 21:54:39 +0200

    1.14. Зарегистрируем команду ImportCommand

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,10 +1,16 @@
-import { CLIApplication, HelpCommand, VersionCommand } from './cli/index.js';
+import {
+  CLIApplication,
+  HelpCommand,
+  ImportCommand,
+  VersionCommand,
+} from './cli/index.js';
 
 function bootstrap() {
   const cliApplication = new CLIApplication();
   cliApplication.registerCommands([
     new HelpCommand(),
-    new VersionCommand()
+    new VersionCommand(),
+    new ImportCommand(),
   ]);
 
   cliApplication.processCommand(process.argv);
