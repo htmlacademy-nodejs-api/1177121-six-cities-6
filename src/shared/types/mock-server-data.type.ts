e56c3f57246f886a8e0eb717456c346a8de39c1c
RayFreedom88 <captain.musin@yandex.ru>
2024-01-22 21:32:45 +0200

    2.5. Добавит тип MockServerData

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,12 @@
+import { ECity } from './city.enum.js';
+
+export type TMockServerData = {
+  titles: string[];
+  descriptions: string[];
+  cities: ECity[];
+  previews: string[];
+  photos: string[];
+  users: string[];
+  emails: string[];
+  avatars: string[];
+};
