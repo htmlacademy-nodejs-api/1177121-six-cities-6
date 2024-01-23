46dd5fe34b28539e1157fa21129636b1144e5a53
RayFreedom88 <captain.musin@yandex.ru>
2024-01-23 18:00:10 +0200

    [WIP] 2.14. Добавит потоки (stream)

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,13 +1,37 @@
 import EventEmitter from 'node:events';
+import { createReadStream } from 'node:fs';
 import { UNICODE } from '../../constants/index.js';
 import { IFileReader } from './file-reader.interface.js';
 
-export class TSVFileReader implements IFileReader {
-  private rawData = '';
+const CHUNK_SIZE = 16384; // 16KB
 
-  constructor(private readonly filename: string) {}
+export class TSVFileReader extends EventEmitter implements IFileReader {
+  constructor(private readonly filename: string) {
+    super();
+  }
 
-  public read(): void {
-    // Код для работы с потоками
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: UNICODE,
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
+    }
+
+    this.emit('end', importedRowCount);
   }
 }
