85f98ecd043b3bd5b704713218793e977a69dbd7
RayFreedom88 <captain.musin@yandex.ru>
2024-01-23 18:41:05 +0200

    2.15 Рефакторит команду ImportCommand

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
 import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
+import { createOffer, getErrorMessage } from '../../shared/helpers/index.js';
 import { ICommand } from './command.interface.js';
 import { Command } from './command.constants.js';
 
@@ -8,20 +9,27 @@ export class ImportCommand implements ICommand {
     return Command.Import;
   }
 
-  public execute(...parameters: string[]): void {
+  private onImportedLine(line: string) {
+    const offer = createOffer(line);
+    console.info(offer);
+  }
+
+  private onCompleteImport(count: number) {
+    console.info(`${count} rows imported.`);
+  }
+
+  public async execute(...parameters: string[]): Promise<void> {
     const [filename] = parameters;
     const fileReader = new TSVFileReader(filename.trim());
 
-    try {
-      fileReader.read();
-      console.log(fileReader.toArray());
-    } catch (err) {
-      if (!(err instanceof Error)) {
-        throw err;
-      }
+    fileReader.on('line', this.onImportedLine);
+    fileReader.on('end', this.onCompleteImport);
 
-      console.error(`Can't import data from file: ${chalk.redBright(filename)}`);
-      console.error(`Details: ${err.message}`);
+    try {
+      await fileReader.read();
+    } catch (error) {
+      console.error(`${chalk.redBright('Error')}: Can't import data from file: ${chalk.redBright(filename)}`);
+      console.error(getErrorMessage(error));
     }
   }
 }
