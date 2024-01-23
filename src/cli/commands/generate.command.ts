02070775367b3a0e107236b74b3d1f6229378cc0
RayFreedom88 <captain.musin@yandex.ru>
2024-01-23 13:08:29 +0200

    [WIP] 2.10. Добавит запрос для получения данных

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,18 +1,38 @@
 
+import got from 'got';
 import { DECIMAL_SYSTEM } from '../../shared/constants/index.js';
+import { TMockServerData } from '../../shared/types/mock-server-data.type.js';
 import { Command } from './command.constants.js';
 import { ICommand } from './command.interface.js';
+import chalk from 'chalk';
 
 export class GenerateCommand implements ICommand {
+  private initialData: TMockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${chalk.redBright(url)}`);
+    }
+  }
+
   public getName(): string {
     return Command.Generate;
   }
 
-  public execute(...parameters: string[]): void {
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, DECIMAL_SYSTEM);
 
-    // Код для получения данных с сервера.
-    // Формирование объявлений.
+    try {
+      await this.load(url);
+    } catch (error: unknown) {
+      console.error('Can\'t generate data');
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
   }
 }
