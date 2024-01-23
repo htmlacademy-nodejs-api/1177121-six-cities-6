c9e3697913ce413ac7f5c62ba917242b4840b13d
RayFreedom88 <captain.musin@yandex.ru>
2024-01-23 18:42:44 +0200

    2.17. Рефакторит GenerateCommand

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,10 +1,11 @@
 
 import got from 'got';
 import chalk from 'chalk';
-import { appendFile } from 'node:fs/promises';
-import { DECIMAL_SYSTEM, UNICODE } from '../../shared/constants/index.js';
+import { DECIMAL_SYSTEM } from '../../shared/constants/index.js';
 import { TMockServerData } from '../../shared/types/mock-server-data.type.js';
 import { TSVOfferGenerator } from '../../shared/libs/offer-generator/index.js';
+import { getErrorMessage } from '../../shared/helpers/index.js';
+import { TSVFileWriter } from '../../shared/libs/file-writer/index.js';
 import { Command } from './command.constants.js';
 import { ICommand } from './command.interface.js';
 
@@ -21,13 +22,10 @@ export class GenerateCommand implements ICommand {
 
   private async write(filepath: string, offerCount: number) {
     const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    const tsvFileWriter = new TSVFileWriter(filepath);
 
     for (let i = 0; i < offerCount; i++) {
-      await appendFile(
-        filepath,
-        `${tsvOfferGenerator.generate()}\n`,
-        { encoding: UNICODE }
-      );
+      await tsvFileWriter.write(tsvOfferGenerator.generate());
     }
   }
 
@@ -42,13 +40,11 @@ export class GenerateCommand implements ICommand {
     try {
       await this.load(url);
       await this.write(filepath, offerCount);
+
       console.info(`${chalk.greenBright('Success')}: File ${chalk.green(filepath)} was created!`);
     } catch (error: unknown) {
       console.error(`${chalk.redBright('Error')}: Can't generate data`);
-
-      if (error instanceof Error) {
-        console.error(error.message);
-      }
+      console.error(getErrorMessage(error));
     }
   }
 }
