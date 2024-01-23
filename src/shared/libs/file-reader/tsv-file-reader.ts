ab03b81f94863251dc2876b702ef643196275810
RayFreedom88 <captain.musin@yandex.ru>
2024-01-23 16:32:29 +0200

    [WIP] 2.12. Рефакторинг. Функция createOffer

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,8 +1,6 @@
-import { IFileReader } from './file-reader.interface.js';
 import { readFileSync } from 'node:fs';
-import { TOffer } from '../../types/index.js';
-import { createOffer } from '../../helpers/index.js';
-import { ErrorMessage, UNICODE } from '../../constants/index.js';
+import { UNICODE } from '../../constants/index.js';
+import { IFileReader } from './file-reader.interface.js';
 
 export class TSVFileReader implements IFileReader {
   private rawData = '';
@@ -11,17 +9,5 @@ export class TSVFileReader implements IFileReader {
 
   public read(): void {
     this.rawData = readFileSync(this.filename, { encoding: UNICODE });
-  }
-
-  public toArray(): TOffer[] {
-    if (!this.rawData) {
-      throw new Error(ErrorMessage.ReadFile);
-    }
-
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim().length > 0)
-      .map((line) => line.split('\t'))
-      .map(createOffer);
   }
 }
