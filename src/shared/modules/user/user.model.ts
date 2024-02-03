0d3490d8ca79071eb83e6d282d5c3390bdf729fb
RayFreedom88 <captain.musin@yandex.ru>
2024-02-03 22:22:55 +0200

    [WIP] 4.12. Расширит интерфейс UserDocument

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,13 +1,16 @@
 import { Schema, Document, model } from 'mongoose';
 import { TUser } from '../../types/index.js';
 
-export interface IUserDocument extends TUser, Document {}
+export interface IUserDocument extends TUser, Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 const userSchema = new Schema({
   name: String,
   email: String,
   avatar: String,
   userType: String,
-});
+}, { timestamps: true });
 
 export const UserModel = model<IUserDocument>('User', userSchema);
