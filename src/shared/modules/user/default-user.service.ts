6c9bd2474051b1a50573e8f56f9c178ffebdc806
RayFreedom88 <captain.musin@yandex.ru>
2024-02-04 19:13:12 +0200

    [WIP] 4.25. Добавит внедрение зависимостей в DefaultUserService

--- src/shared/modules/user/default-user.service.ts
+++ src/shared/modules/user/default-user.service.ts
@@ -1,13 +1,24 @@
-import { IUserService } from './user-service.interface.js';
 import { DocumentType } from '@typegoose/typegoose';
+import { inject, injectable } from 'inversify';
+import { Component } from '../../types/index.js';
+import { ILogger } from '../../libs/logger/index.js';
+import { IUserService } from './user-service.interface.js';
 import { UserEntity, UserModel } from './user.entity.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
 
+@injectable()
 export class DefaultUserService implements IUserService {
+  constructor(
+    @inject(Component.Logger) private readonly logger: ILogger,
+  ) {}
+
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
-    return UserModel.create(user);
+    const result = await UserModel.create(user);
+    this.logger.info(`New user created: ${user.email}`);
+
+    return result;
   }
 }
