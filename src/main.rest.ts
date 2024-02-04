d3ae2b666ae51dcfb99d15df47854d4a54f747d3
RayFreedom88 <captain.musin@yandex.ru>
2024-02-04 18:37:45 +0200

    [WIP] 4.24. Разобьёт общий контейнер

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,19 +1,12 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { ILogger, PinoLogger } from './shared/libs/logger/index.js';
-import { IConfig, RestConfig, TRestSchema } from './shared/libs/config/index.js';
-import { IDatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 import { Component } from './shared/types/index.js';
-import { RestApplication } from './rest/index.js';
+import { RestApplication, createRestApplicationContainer } from './rest/index.js';
 
 async function bootstrap() {
-  const container = new Container();
-  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
-  container.bind<ILogger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<IConfig<TRestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  container.bind<IDatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inRequestScope();
+  const appContainer = Container.merge(createRestApplicationContainer());
 
-  const application = container.get<RestApplication>(Component.RestApplication);
+  const application = appContainer.get<RestApplication>(Component.RestApplication);
   await application.init();
 }
 
