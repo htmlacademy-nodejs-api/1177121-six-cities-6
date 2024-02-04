4aa8f9e02dac8c5c6e248712cc338c88b750394a
RayFreedom88 <captain.musin@yandex.ru>
2024-02-04 23:14:58 +0200

    4.30. Обновит команду import

--- src/cli/commands/command.constants.ts
+++ src/cli/commands/command.constants.ts
@@ -1,5 +1,7 @@
 export const DIR_PATH = 'src/cli/commands';
 export const FILE_EXTENSION = '.command.ts';
+export const DEFAULT_DB_PORT = '27017';
+export const DEFAULT_USER_PASSWORD = '123456';
 
 export const Command = {
   DoubleDash: '--',
