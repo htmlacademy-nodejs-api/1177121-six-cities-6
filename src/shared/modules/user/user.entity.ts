1d135a1a8dc6a967a43a490fa6a980d043b70694
RayFreedom88 <captain.musin@yandex.ru>
2024-02-04 14:41:10 +0200

    [ WIP] 4.17. Добавит определение модели и схемы

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,8 +1,34 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { EUserType, TUser } from '../../types/index.js';
 
 export class UserEntity implements TUser {
-  public name: string;
-  public email: string;
-  public avatar: string;
-  public userType: EUserType;
+  @prop({
+    required: true,
+    minlength: [2, 'Min length for name is 2'],
+    default: '',
+  })
+  public name!: string;
+
+  @prop({
+    required: true,
+    unique: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+  })
+  public email!: string;
+
+  @prop({
+    required: false,
+    match: [/\.(jpg|png)(\?.*)?$/i, 'The avatar image must match the format .jpg or .png'],
+    default: '',
+  })
+  public avatar!: string;
+
+  @prop({
+    required: true,
+    enum: EUserType,
+    default: EUserType.Standard,
+  })
+  public userType!: EUserType;
 }
+
+export const UserModel = getModelForClass(UserEntity);
