9ecef20ab176cc2bdaf351c8f1049b1b643d4ed8
RayFreedom88 <captain.musin@yandex.ru>
2024-02-04 16:12:53 +0200

    [WIP] 4.20. Добавит методы в UserEntity

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
-import { defaultClasses, getModelForClass, prop } from '@typegoose/typegoose';
+import {
+  defaultClasses,
+  getModelForClass,
+  modelOptions,
+  prop,
+} from '@typegoose/typegoose';
 import { EUserType, TUser } from '../../types/index.js';
-
+import { createSHA256 } from '../../helpers/index.js';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface UserEntity extends defaultClasses.Base {}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'users',
+    timestamps: true,
+  },
+})
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class UserEntity extends defaultClasses.TimeStamps implements TUser {
@@ -17,13 +29,19 @@ export class UserEntity extends defaultClasses.TimeStamps implements TUser {
   @prop({
     required: true,
     unique: true,
-    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    match: [
+      /^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+      'Email is incorrect'
+    ],
   })
   public email!: string;
 
   @prop({
     required: false,
-    match: [/\.(jpg|png)(\?.*)?$/i, 'The avatar image must match the format .jpg or .png'],
+    match: [
+      /\.(jpg|png)(\?.*)?$/i,
+      'The avatar image must match the format .jpg or .png',
+    ],
     default: '',
   })
   public avatar!: string;
@@ -34,6 +52,26 @@ export class UserEntity extends defaultClasses.TimeStamps implements TUser {
     default: EUserType.Standard,
   })
   public userType!: EUserType;
+
+  @prop({ required: true, default: '' })
+  private password?: string;
+
+  constructor(userData: TUser) {
+    super();
+
+    this.name = userData.name;
+    this.email = userData.email;
+    this.avatar = userData.avatar;
+    this.userType = userData.userType;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt);
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const UserModel = getModelForClass(UserEntity);
