3f89180e25fb067884230b2a051bf726964afdce
RayFreedom88 <captain.musin@yandex.ru>
2024-02-04 13:09:34 +0200

    [WIP] 4.14. Задействует встроенную валидацию

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,19 +1,36 @@
 import { Schema, Document, model } from 'mongoose';
-import { TUser } from '../../types/index.js';
+import { EUserType, TUser } from '../../types/index.js';
 
 export interface IUserDocument extends TUser, Document {
   createdAt: Date;
   updatedAt: Date;
 }
 
-const userSchema = new Schema({
-  name: String,
-  email: {
-    type: String,
-    unique: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: [2, 'Min length for name is 2'],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    },
+    avatar: {
+      type: String,
+      required: false,
+      match: [/\.(jpg|png)(\?.*)?$/i, 'The avatar image must match the format .jpg or .png']
+    },
+    userType: {
+      type: String,
+      required: true,
+      enum: EUserType,
+    },
   },
-  avatar: String,
-  userType: String,
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 export const UserModel = model<IUserDocument>('User', userSchema);
