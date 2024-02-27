dca35965161ad7979e1030eecd3912352a889551
RayFreedom88 <captain.musin@yandex.ru>
2024-02-27 23:07:51 +0200

    7.5. Расширит интерфейс Route

--- src/shared/libs/rest/types/route.interface.ts
+++ src/shared/libs/rest/types/route.interface.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { EHttpMethod } from './http-method.enum.js';
+import { IMiddleware } from '../index.js';
 
 export interface IRoute {
   path: string;
   method: EHttpMethod;
   handler: (req: Request, res: Response, next: NextFunction) => void;
+  middlewares?: IMiddleware[];
 }
