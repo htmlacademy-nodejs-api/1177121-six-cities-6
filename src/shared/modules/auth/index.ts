40584a08c368ba433670962522e50da3c0e7864e
RayFreedom88 <captain.musin@yandex.ru>
2024-03-04 21:21:48 +0200

    [WIP] 8.8. Подготовим контейнер зависимостей для модуля Auth

--- src/shared/modules/auth/index.ts
+++ src/shared/modules/auth/index.ts
@@ -4,3 +4,4 @@ export * from './default-auth.service.js';
 export * from './errors/index.js';
 export * from './types/index.js';
 export * from './auth.exception-filter.js';
+export * from './auth.container.js';
