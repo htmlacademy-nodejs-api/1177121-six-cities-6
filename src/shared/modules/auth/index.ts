4030ea5c511be1d3a8004ab6b634a427811c8c5f
RayFreedom88 <captain.musin@yandex.ru>
2024-03-04 20:48:34 +0200

    [WIP] 8.6. Новые ошибки UserNotFoundException и UserPasswordIncorrectException

---
+++ src/shared/modules/auth/index.ts
@@ -0,0 +1,5 @@
+export * from './auth-service.interface.js';
+export * from './auth.constant.js';
+export * from './default-auth.service.js';
+export * from './errors/index.js';
+export * from './types/index.js';
